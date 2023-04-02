ed76c147fb259982bc25da6dd9148881040f90e6
jeffgat <jeff.gtbn@gmail.com>
2023-04-02 14:34:30 -0600

    new files for new routes

--- t3/src/pages/index.tsx
+++ t3/src/pages/index.tsx
@@ -9,6 +9,7 @@ import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import Link from "next/link";
 
 dayjs.extend(relativeTime);
 
@@ -86,8 +87,12 @@ const PostView = ({ post, author }: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-400">
-          <span>{`@${author.username}`}</span>
-          <span>{` · ${dayjs(post.createdAt).fromNow()}`}</span>
+          <Link href={`/@${author.username}`}>
+            <span>{`@${author.username}`}</span>
+          </Link>
+          <Link href={`/post/${post.id}`}>
+            <span>{` · ${dayjs(post.createdAt).fromNow()}`}</span>
+          </Link>
         </div>
         <span className="text-2xl">{post.content}</span>
       </div>
